ddde92afc0c5504c1347080dd0d527bf33b797c4
StevenDelval <asteven59220@free.fr>
2023-10-25 11:38:45 +0200

    test : add test

--- real_estate_app/main_app/test_main_app.py
+++ real_estate_app/main_app/test_main_app.py
@@ -1,6 +1,40 @@
-import pytest
+from django.contrib.auth import get_user_model
 from django.urls import reverse
+from django.test import Client, TestCase
+from django.contrib.auth.models import User
+import pytest
 from .utils import make_prediction
+
+class LogoutViewTest(TestCase):
+    def test_logout_view(self):
+        # Créez un utilisateur de test
+        user = User.objects.create_user(username='testuser', password='testpassword')
+
+        # Connectez l'utilisateur
+        self.client.login(username='testuser', password='testpassword')
+
+        # Accédez à la vue de déconnexion
+        response = self.client.get(reverse('logout'))
+
+        # Vérifiez que l'utilisateur est déconnecté (redirigé vers la page d'accueil)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('index'))
+
+        # Vérifiez que l'utilisateur est effectivement déconnecté
+        user = get_user_model().objects.get(username='testuser')
+        self.assertFalse(user.is_authenticated)
+class IndexViewTest(TestCase):
+    def test_index_view(self):
+        # Assurez-vous que l'utilisateur est connecté
+        self.client.login(username='testuser', password='testpassword')
+
+        # Accédez à la vue index
+        response = self.client.get(reverse('index'))
+
+        # Vérifiez que la page s'affiche correctement
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
 
 
 @pytest.mark.django_db
